7f913a0788cedace46f9b0bdd13422252b2ca6fd
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-03-08 23:31:23 -0700

    Serve frontend from root directory

--- Webserver/server/routes/admin.go
+++ Webserver/server/routes/admin.go
@@ -14,10 +14,6 @@ func (rs AdminRoutes) Routes() chi.Router {
 	// TODO: Soooome kind of auth middleware.
 	// It'll probably be something super basic, probably no roles/ granular permissions
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world from admin"))
-	})
-
 	// Sub routes for indexes actions
 	r.Mount("/index", IndexRoutes{}.Routes())
 
--- Webserver/server/routes/main.go
+++ Webserver/server/routes/main.go
@@ -1,7 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,12 +15,33 @@ type MainRouter struct{}
 func (rs MainRouter) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("main root router"))
-	})
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "../", "client"))
+	fmt.Println(filesDir)
+
+	FileServer(r, "/", filesDir)
 
 	r.Mount("/admin", AdminRoutes{}.Routes())
 	r.Mount("/search", SearchRoutes{}.Routes())
 
 	return r
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
