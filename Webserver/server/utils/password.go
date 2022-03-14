a8d9d6ef9a3342ba2ec69d2fca1fef0e10f7f05b
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-03-13 21:25:51 -0700

    Add password validation

--- Webserver/server/utils/password.go
+++ Webserver/server/utils/password.go
@@ -9,3 +9,9 @@ func HashPassword(password string) (string, error) {
 
 	return string(bytes), err
 }
+
+func ValidatePassword(hash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
+}
