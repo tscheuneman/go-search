2a062d2f033403d83876371e0b2ae2dffc12f176
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-03-14 22:49:06 -0700

    Remove the local file server

--- Webserver/server/routes/main.go
+++ Webserver/server/routes/main.go
@@ -1,11 +1,6 @@
 package routes
 
 import (
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/tscheuneman/go-search/controllers"
 )
@@ -15,34 +10,10 @@ type MainRouter struct{}
 func (rs MainRouter) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	workDir, _ := os.Getwd()
-	filesDir := http.Dir(filepath.Join(workDir, "../", "client/build"))
-
-	FileServer(r, "/", filesDir)
-
 	r.Mount("/admin", AdminRoutes{}.Routes())
 	r.Mount("/search", SearchRoutes{}.Routes())
 
 	r.Post("/login", controllers.Login)
 
 	return r
-}
-
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
 }
