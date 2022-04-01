d14fede274958d15ff5d28f4b0c419e98adc4d31
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-03-31 20:05:41 -0700

    Re-enable auth middleware.  This got disabled by mistake

--- Webserver/server/middleware/auth.go
+++ Webserver/server/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 
+	"github.com/tscheuneman/go-search/container"
 	"github.com/tscheuneman/go-search/utils"
 )
 
@@ -15,24 +17,22 @@ func JwtMiddleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 
-		/*
-			authCookie, err := r.Cookie(container.AUTH_COOKIE)
-			if err != nil {
-				render.Render(w, r, utils.ErrForbiddenRequest(errors.New("Auth Token Missing")))
-				return
-			}
-			validToken, user_id := utils.ValidateToken(authCookie.Value)
-			if !validToken {
-				render.Render(w, r, utils.ErrForbiddenRequest(errors.New("Invalid Auth Token")))
-				return
-			}
+		authCookie, err := r.Cookie(container.AUTH_COOKIE)
+		if err != nil {
+			render.Render(w, r, utils.ErrForbiddenRequest(errors.New("Auth Token Missing")))
+			return
+		}
+		validToken, user_id := utils.ValidateToken(authCookie.Value)
+		if !validToken {
+			render.Render(w, r, utils.ErrForbiddenRequest(errors.New("Invalid Auth Token")))
+			return
+		}
 
-			ctx := r.Context()
-			ctx = context.WithValue(ctx, "UserID", user_id)
-			r = r.WithContext(ctx)
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "UserID", user_id)
+		r = r.WithContext(ctx)
 
-			next.ServeHTTP(w, r)
-		*/
+		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
 }
